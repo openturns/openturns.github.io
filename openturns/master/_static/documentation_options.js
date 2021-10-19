ac2bd484bd34f21b448d9e902afff4f9a2cbe9d4
CircleCI <sayhi@circleci.com>
2021-10-19 17:40:11 +0000

    CircleCI build openturns 8166

--- openturns/master/_static/documentation_options.js
+++ openturns/master/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '1.18dev',
+    VERSION: '1.19dev',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
