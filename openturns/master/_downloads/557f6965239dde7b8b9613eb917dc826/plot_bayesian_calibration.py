b8e1e7d86c116cbf85a01e39bc47223ab03e0d46
CircleCI <sayhi@circleci.com>
2023-05-10 09:36:55 +0000

    CircleCI build openturns 13268

--- openturns/master/_downloads/557f6965239dde7b8b9613eb917dc826/plot_bayesian_calibration.py
+++ openturns/master/_downloads/557f6965239dde7b8b9613eb917dc826/plot_bayesian_calibration.py
@@ -231,13 +231,6 @@ for mh in mh_coll:
 sampler = ot.Gibbs(mh_coll)
 
 # %%
-# Tuning of the Gibbs algorithm:
-
-# %%
-sampler.setThinning(1)
-sampler.setBurnIn(2000)
-
-# %%
 # Generate a sample from the posterior distribution of the parameters :math:`\vect \theta`.
 
 # %%
