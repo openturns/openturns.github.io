cb2dd3ebdbebdb4ca4dcc7588fb553048ffb636e
CircleCI <sayhi@circleci.com>
2024-02-20 18:12:59 +0000

    CircleCI build openturns 16184

--- openturns/master/_downloads/66d92db0918a7118e745f79301601701/design_experiment-5.py
+++ openturns/master/_downloads/66d92db0918a7118e745f79301601701/design_experiment-5.py
@@ -2,7 +2,7 @@ import openturns as ot
 from openturns.viewer import View
 
 # MonteCarlo
-d = ot.MonteCarloExperiment(ot.ComposedDistribution([ot.Uniform()]*3), 32)
+d = ot.MonteCarloExperiment(ot.JointDistribution([ot.Uniform()]*3), 32)
 s = d.generate()
 s.setDescription(["X1", "X2", "X3"])
 g = ot.VisualTest.DrawPairs(s)
